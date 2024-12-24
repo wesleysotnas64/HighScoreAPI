0ab1b80ca8ce581cf5c9f2b38f4ce99575059671
wesleysotnas64 <wesleysotnas64@gmail.com>
2024-12-23 21:25:34 -0300

    Config: Permitindo uso do Swagger fora do ambiente de desenvolvimento.

--- Program.cs
+++ Program.cs
@@ -20,11 +20,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
